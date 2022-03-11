7cdc5acdee4a867d2eaf2aa460d9ebd7731f4f82
Austin Mestayer <austinmestayer@outlook.com>
2022-03-10 20:14:45 -0600

    homepage routing changed

--- src/PortfolioContainer/Home/Header/Header.js
+++ src/PortfolioContainer/Home/Header/Header.js
@@ -1,15 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {TOTAL_SCREENS, GET_SCREEN_INDEX} from '../../../utilities/commonUtils'
 import ScrollService from '../../../utilities/ScrollService'
-import {faBars} from '@fortawesome/react-fontawesome'
+import {faBars} from '@fortawesome/free-solid-svg-icons'
+import {FondAwesomeIcon} from '@fortawesome/react-fontawesome'
+import './Header.css'
 
+/*
+Header taskbar function declarations 
+*/
 
+export default function () {
+    const [selectedScreen, setSelectedScreen] = useState(0)
+    const [showHeaderOptions, setShowHeaderOptions] = useState(false)
 
-export default function 
-() {
-  return (
-    <div>
+    const updateCurrentScreen = (currentScreen) => {
+        if (!currentScreen || !currentScreen.screenInView) 
+            return;
+        
+        let screenIndex = GET_SCREEN_INDEX(currentScreen.screenInView)
+        if (screenIndex < 0) 
+            return;
+        
+    }
 
-    </div>
-  )
+    let currentScreenSubscription = ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen);
+
+    const getHeaderOptions = () => {
+        return(
+          TOTAL_SCREENS.map((screen, i)=>(
+                <div key = {screen.screen_name} className = {getHeaderOptionsClass(i)}
+                onClick = {() => switchScreen(i, screen)}>
+                    <span>{screen.screen_name}</span>
+                  </div>
+          ))
+        )
+    }
+
+    const getHeaderOptionsClass = (index) =>{
+      let classes = "header-option";
+      if(index < TOTAL_SCREENS.length - 1)
+        classes += "header-option-separator";
+
+          if(selectedScreen === index)
+            classes += "selected-header-option";
+            return 
+    }
+
+    return (
+        <div></div>
+    )
 }
